f742594412db028c435f046c2438d121f0ed71b6
Ágota Fábián <agota.fabian16@gmail.com>
2025-09-10 15:48:53 +0200

    Delete spec files and unused component

--- src/app/receive-parcel/receive-parcel.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-receive-parcel',
-  imports: [],
-  templateUrl: './receive-parcel.component.html',
-  styleUrl: './receive-parcel.component.less',
-})
-export class ReceiveParcelComponent {}
--- src/app/shared/pipes/curry.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CurryPipe } from './curry.pipe';
-
-describe('CurryPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CurryPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
